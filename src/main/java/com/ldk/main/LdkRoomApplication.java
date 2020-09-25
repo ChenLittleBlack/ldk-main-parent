ea134cf065e667b02fd57a6db3a16b0988718788
陈小黑 <191714375@qq.com>
2020-09-25 15:38:08 +0800

    feat: 初始化项目

---
+++ src/main/java/com/ldk/main/LdkRoomApplication.java
@@ -0,0 +1,17 @@
+package com.ldk.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author czl
+ * @date 2020/09/25 15:20
+ */
+@SpringBootApplication
+public class LdkRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdkRoomApplication.class, args);
+    }
+
+}
