d3d396c09f44c9dc3a7d8f498a066c7c8ab513b8
ChenLittleBlack <191714375@qq.com>
2020-09-27 10:51:09 +0800

    feat: 启动文件调整

--- ldk-main-webapps/src/main/java/com/ldk/main/LdkRoomApplication.java
+++ ldk-main-webapps/src/main/java/com/ldk/main/LdkRoomApplication.java
@@ -1,17 +1,27 @@
 package com.ldk.main;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.TimeZone;
 
 /**
  * @author czl
  * @date 2020/09/25 15:20
  */
 @SpringBootApplication
+@Slf4j
 public class LdkRoomApplication {
 
+
     public static void main(String[] args) {
-        SpringApplication.run(LdkRoomApplication.class, args);
+        // 时区修正为上海
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        ApplicationContext ctx = SpringApplication.run(LdkRoomApplication.class, args);
+        log.info("####### [项目启动] bean总数:{} #######", ctx.getBeanDefinitionCount());
     }
 
 }
