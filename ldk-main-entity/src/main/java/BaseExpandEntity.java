8795e5ccf12392310aa53e15bafc67362463deb9
ChenLittleBlack <191714375@qq.com>
2020-09-27 16:42:55 +0800

    feat: 基础扩展实体

---
+++ ldk-main-entity/src/main/java/BaseExpandEntity.java
@@ -0,0 +1,60 @@
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author czl
+ * @description 基础扩展实体
+ * @createDate 2020/09/27 16:16
+ */
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+public class BaseExpandEntity implements Serializable {
+
+    /**
+     * 创建人
+     */
+    @CreatedBy
+    private Long creator;
+
+    /**
+     * 创建时间
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreatedDate
+    private Date createTime;
+
+    /**
+     * 修改人
+     */
+    @LastModifiedBy
+    private Long modifier;
+
+    /**
+     * 修改时间
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @LastModifiedDate
+    private Date modifyTime;
+
+    /**
+     * 删除标志
+     */
+    private Integer removeFlag;
+
+}
